0d063a1d5599b10f2805370486aac75444ed85bb
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-02 22:06:57 +0600

    Updates

--- Snippets/test.cpp
+++ Snippets/test.cpp
@@ -63,56 +63,6 @@ typedef set<double> sd;
 
 void Boom()
 {
-    ll n;
-    string s;
-    cin >> n >> s;
-
-    if ((s[0] != 'm' and s[0] != 'M') || (s[s.size() - 1] != 'w' and s[s.size() - 1 != 'W']))
-    {
-        no;
-        return;
-    }
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (s[i] != s[i + 1])
-        {
-            if ((s[i] == 'm' || s[i] == 'M') && (s[i + 1] == 'e' || s[i + 1] == 'E'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'e' || s[i] == 'E') && (s[i + 1] == 'o' || s[i + 1] == 'O'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'm' || s[i] == 'M') && (s[i + 1] == 'm' || s[i + 1] == 'M'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'o' || s[i] == 'O') && (s[i + 1] == 'w' || s[i + 1] == 'W'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'e' || s[i] == 'E') && (s[i + 1] == 'e' || s[i + 1] == 'E'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'o' || s[i] == 'O') && (s[i + 1] == 'o' || s[i + 1] == 'O'))
-            {
-                continue;
-            }
-            else if ((s[i] == 'w' || s[i] == 'W') && (s[i + 1] == 'w' || s[i + 1] == 'W'))
-            {
-                continue;
-            }
-            else
-            {
-                no;
-                return;
-            }
-        }
-    }
-    yes;
 }
 
 int main()
