83d17546f641f36853ef98cbae68d7742d2e6d18
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-03 11:50:53 +0600

    Updates

--- VS Code Items/Snippets/Direct/Current.cpp
+++ VS Code Items/Snippets/Direct/Current.cpp
@@ -26,49 +26,25 @@ typedef set<string> ss;
 
 #define pb push_back
 #define mp make_pair
-#define pob pop_back
 #define in insert
 #define fi first
 #define se second
 #define space ' '
 #define endl "\n"
-#define lb lower_bound
-#define ub upper_bound
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 
 #define b() begin()
 #define e() end()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
-#define sz(x) (int)x.size()
 #define vecMax(data) *max_element(data.begin(), data.end())
 #define vecMin(data) *min_element(data.begin(), data.end())
 #define vecSum(data) accumulate(data.begin(), data.end(), 0)
-#define vecCount(data, key) count(data.begin(), data.end(), key)
-#define Reverse(data) reverse(data.begin(), data.end())
-
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 
 #define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
 #define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
-#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
-#define setp(n) fixed << setprecision(n)
-#define mem(arr, val) memset(arr, val, sizeof(arr));
 #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) / gcd(a, b) * (b))
-#define sqr(a) ((a) * (a))
-#define MOD 1000000007
-
-const double PI = acos(-1);
-const int mx = 1e9 + 7;
-const double eps = 1e-9;
-const int inf = 2000000000;
-const ll infLL = 9000000000000000000;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
