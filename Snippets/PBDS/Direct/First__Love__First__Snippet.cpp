809b8581e1d41b6a617714c8d1ecdb1dcde31896
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 00:58:19 +0600

    Update

---
+++ Snippets/PBDS/Direct/First__Love__First__Snippet.cpp
@@ -0,0 +1,117 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+
+#define vi vector<ll>
+#define vd vector<double>
+#define vs vector<string>
+#define vc vector<char>
+#define vvi vector<vector<ll>>
+#define vpii vector<pair<ll, ll>>
+#define vpsi vector<pair<string, ll>>
+#define pii pair<ll, ll>
+#define psi pair<string, ll>
+#define pis pair<ll, string>
+#define mii map<ll, ll>
+#define mib map<ll, bool>
+#define msi map<string, ll>
+#define mis map<ll, string>
+#define pdd pair<double, double>
+#define si set<ll>
+#define ss set<string>
+#define sd set<double>
+#define pq priority_queue<ll>
+#define pqs priority_queue<string>
+#define pqg priority_queue<ll, vi, greater<ll>>
+
+#define vit vector<int>::iterator
+#define sit set<int>::iterator
+
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+
+#define mid(l, r) ((r + l) / 2)
+#define left(node) (node * 2)
+#define right(node) (node * 2 + 1)
+#define mx_int_prime 999999937
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define sz(x) (int)x.size()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
+#define Reverse(data) reverse(data.begin(), data.end())
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
+#define setp(n) fixed << setprecision(n)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define sqr(a) ((a) * (a))
+#define MOD 1000000007
+
+const double PI = acos(-1);
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    // Let's Move
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // cout<<"Case "<<t<<" : ";
+        Boom();
+    }
+
+    return 0;
+}
---
+++ Snippets/PBDS/Direct/basic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#define ll long long
+#define scan(data) scanf("%d", &data)
+#define print(data) printf("%d\n", data)
+#define fscan(data) scanf("%f", &data)
+#define fprint(data) printf("%.2f\n", data)
+
+int main()
+{
+
+    return 0;
+}
