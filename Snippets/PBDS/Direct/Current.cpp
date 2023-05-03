28fe88b516d938302e9fd59e8feb104e7c638df4
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 01:01:34 +0600

    Update

--- Snippets/PBDS/Direct/Current.cpp
+++ Snippets/PBDS/Direct/Current.cpp
@@ -6,6 +6,9 @@ author = Hafiz_Sakib;
 
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
 typedef long long int ll;
@@ -23,6 +26,19 @@ typedef map<ll, ll> mii;
 typedef map<string, ll> msi;
 typedef set<ll> si;
 typedef set<string> ss;
+
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+typedef tree<ll, null_type, greater<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    greater_set;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    multi_set;
+typedef tree<ll, null_type, greater_equal<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    greater_multi_set;
 
 #define pb push_back
 #define mp make_pair
@@ -45,11 +61,13 @@ typedef set<string> ss;
 #define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
 #define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
 #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-const double eps = 1e-12;
-const int mx = 1e8 + 123;
 
 #define debug(x) cerr << x << endl;
 #define here fprintf(stderr, "====I am Here====\n");
+#define setp(n) fixed << setprecision(n)
+
+const double eps = 1e-12;
+const int mx = 1e8 + 123;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
