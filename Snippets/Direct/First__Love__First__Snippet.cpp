3fb6b2c9eac75e974b585e7093979066cd68ee09
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-13 14:50:26 +0600

    DBG

--- Snippets/Direct/First__Love__First__Snippet.cpp
+++ Snippets/Direct/First__Love__First__Snippet.cpp
@@ -95,41 +95,6 @@ const ll infLL = 9000000000000000000;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-bitset<mx> is_prime;
-vector<int> primes;
-
-void sieve(int n)
-{
-    n += 100;
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        is_prime[i] = 1;
-    }
-
-    for (int i = 3; (i * i) <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            for (int j = (i * i); j <= n; j += (i + i))
-            {
-                is_prime[j] = 0;
-            }
-        }
-    }
-
-    is_prime[1] = 0;
-    is_prime[2] = 1;
-    primes.push_back(2);
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            primes.push_back(i);
-        }
-    }
-}
 
 void Boom()
 {
